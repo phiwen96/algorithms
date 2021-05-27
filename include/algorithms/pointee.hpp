f395a069f84b5838538083279ef2cb9dbec1ea8b
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-05-27 10:24:37 +0200

    a

--- include/algorithms/pointee.hpp
+++ include/algorithms/pointee.hpp
@@ -9,86 +9,30 @@ struct pointee;
 
 
 template <typename T>
-struct pointee <T>// : sequence <T>, allocator <T>
+struct pointee <T>
 {
-#define EXPAND \
-    auto i = m_i - m_begin; \
-    m_end += i * 2; \
-    allocator::reallocate (m_begin, m_end); \
-    m_i = m_begin + i;
-    
-#define MAYBE_EXPAND \
-    if (m_i >= m_end) {EXPAND}
-    
-//    using allocator = allocator <T>;
     using value_type = T;
     using pointer = value_type*;
     using reference = value_type&;
     using difference = ptrdiff_t;
-    
-//    using sequence <T>::m_begin;
-//    using sequence <T>::m_end;
-//    using sequence <T>::m_current;
-//    using sequence <T>::active;
-//    using sequence <T>::passive;
-//    sequence <T> m_seq;
-//    pointer m_i;
-    
-    
-    
-//    pointee () //: m_i {m_begin + 1}
-//    {
-////        EXPAND
-////        ++m_i;
-//    }
-    
+
     pointer begin;
     pointer end;
     pointer now;
     
-    template <typename U>
-    auto operator= (U && a) -> void
-    requires requires (value_type& v) {
-        {v = std::forward <U> (a)};
-    }
-    {
-//        MAYBE_EXPAND
-        
-//        ::new (m_i) value_type {std::forward <U> (a)};
-    }
+    auto active () const noexcept       -> size_t;
+    auto passive () const noexcept      -> size_t;
     
-    auto operator* () -> reference
-    {
-//        MAYBE_EXPAND
-        
-        return *now;
-    }
+    auto operator* () noexcept          -> reference;
     
-//    auto operator&()
-//    {
-//        if (m_i >= m_end)
-//        {
-//            auto i = m_i - m_begin;
-//            m_end += 1 + i * 2;
-//            allocator::reallocate (m_begin, m_end);
-//
-//            m_i = m_begin + i;
-//        }
-//
-//        return m_i;
-//    }
+    auto operator-  (int i) noexcept    -> auto&;
+    auto operator+= (auto n) noexcept   -> auto&;
+    auto operator-= (auto n) noexcept   -> auto&;
+    auto operator++ () noexcept         -> auto&;
+    auto operator++ (int) noexcept      -> auto&;
+    auto operator-- () noexcept         -> auto&;
+    auto operator-- (int) noexcept      -> auto&;
     
-    
-    
-    auto operator- (int i) -> auto&;
-    auto operator+= (auto n) -> auto&;
-    auto operator-= (auto n) -> auto&;
-    auto operator++ () -> auto&;
-    auto operator++ (int) -> auto&;
-    auto operator-- () -> auto&;
-    auto operator-- (int) -> auto&;
-    auto active () const -> size_t;
-    auto passive () const -> size_t;
     friend std::ostream& operator<< (std::ostream& os, pointee const& p);
 };
 
@@ -96,151 +40,95 @@ struct pointee <T>// : sequence <T>, allocator <T>
 
 
 
-template <typename T, template <size_t...> typename Size, size_t N>
-struct pointee <T, Size <N>> : sequence <T, Size <N>>, allocator <T>
+
+
+
+
+
+
+
+
+
+template <typename T>
+auto pointee <T>::operator* () noexcept -> reference
 {
-#define EXPAND \
-    auto i = m_i - m_begin; \
-    m_end += i * 2; \
-    allocator::reallocate (m_begin, m_end); \
-    m_i = m_begin + i;
-    
-#define MAYBE_EXPAND \
-    if (m_i >= m_end) {EXPAND}
-    
-    
-    using sequence = sequence <T, Size <N>>;
-    using allocator = allocator <T>;
-    using value_type = T;
-    using pointer = value_type*;
-    using reference = value_type&;
-    using difference = ptrdiff_t;
-    
-    using sequence::m_begin;
-    using sequence::m_end;
-    using sequence::m_current;
-    using sequence::active;
-    using sequence::passive;
-    
-    pointer m_i;
-    
-    pointee () : m_i {m_begin + 1}
+    return *now;
+}
+
+template <typename T>
+auto pointee <T>::operator++ () noexcept -> auto&
+{
+    ++now;
+    return *this;
+}
+
+template <typename T>
+auto pointee <T>::operator++ (int) noexcept -> auto&
+{
+    now++;
+    return *this;
+}
+
+template <typename T>
+auto pointee <T>::operator-- () noexcept -> auto&
+{
+    --now;
+    return *this;
+}
+
+template <typename T>
+auto pointee <T>::operator-- (int) noexcept -> auto&
+{
+    now--;
+    return *this;
+}
+
+template <typename T>
+auto pointee <T>::operator- (int i) noexcept -> auto&
+{
+    return *this;
+}
+
+template <typename T>
+auto pointee <T>::operator+= (auto n) noexcept -> auto&
+{
+    now += n;
+    return *this;
+}
+
+template <typename T>
+auto pointee <T>::operator-= (auto n) noexcept -> auto&
+{
+    now -= n;
+    return *this;
+}
+
+template <typename T>
+auto pointee <T>::active () const noexcept -> size_t
+{
+    return end - begin;
+}
+
+template <typename T>
+auto pointee <T>::passive () const noexcept -> size_t
+{
+    return end - now;
+}
+
+
+template <typename T>
+inline std::ostream& operator<< (std::ostream& os, pointee <T> const& p)
+{
+    for (auto i = p.m_begin; i < p.m_end; ++i)
     {
-        EXPAND
+        os << *i << " ";
     }
-    
-    template <typename U>
-    auto operator= (U && a) -> void
-    requires requires (value_type& v) {
-        {v = std::forward <U> (a)};
-    }
-    {
-        MAYBE_EXPAND
-        
-        ::new (m_i) value_type {std::forward <U> (a)};
-    }
-    
-    auto operator* () -> reference
-    {
-        MAYBE_EXPAND
-        
-        return *m_i;
-    }
-    
-    auto operator&()
-    {
-        if (m_i >= m_end)
-        {
-            auto i = m_i - m_begin;
-            m_end += 1 + i * 2;
-            allocator::reallocate (m_begin, m_end);
-    
-            m_i = m_begin + i;
-        }
-        
-        return m_i;
-    }
-    
-    auto operator-> () -> pointer
-    {
-        return m_current;
-    }
-    
-    friend auto operator- (pointee& p, int i)
-    {
-        return pointee <T> {{p.m_begin, p.m_end, p.m_current - i}};
-    }
-    
-    friend auto operator+= (pointee& p, size_t n) -> auto&
-    {
-        p.m_i += n;
-        return p;
-    }
-    
-    friend auto operator-= (pointee& p, size_t n) -> auto&
-    {
-        p.m_i -= n;
-        return p;
-    }
-    
-    auto operator() (auto&&... a) -> auto&
-    {
-        MAYBE_EXPAND
-        
-        auto curr = m_i - sizeof... (a);
-        
-        ((::new (curr++) value_type {std::forward <decltype (a)>}), ...);
-        return *this;
-    }
-    
-    
-    
-    auto operator++ () -> auto&
-    {
-        ++m_i;
-        return *this;
-    }
-    auto operator++ (int) -> auto&
-    {
-        m_i++;
-        return *this;
-    }
-    auto operator-- () -> auto&
-    {
-        --m_i;
-        return *this;
-    }
-    auto operator-- (int) -> auto&
-    {
-        m_i--;
-        return *this;
-    }
-    
-    auto active () const -> size_t
-    {
-        return m_end - m_begin;
-    }
-    
-    auto capacity () const -> size_t
-    {
-        return m_end - m_begin;
-    }
-    
-    auto passive () const -> size_t
-    {
-        return m_end - m_current;
-    }
-    
-    friend std::ostream& operator<< (std::ostream& os, pointee const& p)
-    {
-        for (auto i = p.m_begin; i < p.m_end; ++i)
-        {
-            os << *i << " ";
-        }
-        return os;
-    }
-    
-};
+    return os;
+}
+
+
+
+
 
 
 
@@ -253,3 +141,26 @@ struct pointee <T, Size <N>> : sequence <T, Size <N>>, allocator <T>
 //        ((::new (curr++) value_type {std::forward <decltype (a)>}), ...);
 //        return *this;
 //}
+
+
+
+//template <typename U>
+//auto operator= (U && a) -> void
+//requires requires (value_type& v) {
+//    {v = std::forward <U> (a)};
+//}
+//{
+//    MAYBE_EXPAND
+//
+//    ::new (m_i) value_type {std::forward <U> (a)};
+//}
+
+
+//#define EXPAND \
+//    auto i = m_i - m_begin; \
+//    m_end += i * 2; \
+//    allocator::reallocate (m_begin, m_end); \
+//    m_i = m_begin + i;
+//
+//#define MAYBE_EXPAND \
+//    if (m_i >= m_end) {EXPAND}
--- tests/src/test_pointee.cpp
+++ tests/src/test_pointee.cpp
@@ -1,75 +1,60 @@
-//#include "testlib.hpp"
-
 #ifdef RUN_CATCH
 
-//#include "test.hpp"
 #include <catch2/catch.hpp>
-#include <algorithms/sequence.hpp>
-#include <algorithms/allocator.hpp>
-#include <algorithms/array.hpp>
-#include <algorithms/size.hpp>
-
-
-//using namespace std;
+#include <algorithms/pointee.hpp>
 
 
 
+int a [10] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+#define INIT(now) \
+    auto p = pointee <int> {a, a + 10, now};
 
 
-
-
-
-
-
-template <int a[6]>
-struct A
+TEST_CASE ("dereference")
 {
-    inline static int aa [10] {};
-    cexp int * const begin {aa+1};
-    cexp int * end {begin + 6};
-};
-
-
-TEST_CASE ("static array")
-{
-    SECTION ("construction")
-    {
-        SECTION ("default")
-        {
-            
-            constexpr int * a {};
-            constexpr auto a0 = A <a> {};
-            constexpr int * i1 {a};
-          
-            
-//            pp1 = i2;
-            
-            
-            constexpr int aa[10] {};
-//            constexpr int* ptr = std::decay_t <decltype (aa)> {aa};
-//            constexpr auto s0 = sequence <int, size <10>> {aa, aa + 10};
-            
-            constexpr auto s1 = sequence <int> {};
-//            p = 10;
-        
-//            std::cout << *p << std::endl;
-//            auto a = array <int, size <10>> {};
-        }
-        
-        SECTION ("value")
-        {
-//            auto a = array <int, size <10>> {1, 2, 3};
-        }
-    }
+    INIT (a)
+    
+    REQUIRE (*p == 0);
 }
 
-TEST_CASE ("dynamic array")
+TEST_CASE ("++")
 {
-    SECTION ("push back")
-    {
-//        auto a = array <int> {};
-//        REQUIRE ((size_t) a == 2);
-    }
+    INIT (a)
+    
+    ++p;
+    REQUIRE (*p == 1);
+    
+    p++;
+    REQUIRE (*p == 2);
 }
+
+TEST_CASE ("--")
+{
+    INIT (a + 2)
+    
+    --p;
+    REQUIRE (*p == 1);
+    
+    p--;
+    REQUIRE (*p == 0);
+}
+
+TEST_CASE ("+=")
+{
+    INIT (a)
+    
+    p += 2;
+    REQUIRE (*p == 2);
+}
+
+TEST_CASE ("-=")
+{
+    INIT (a + 2)
+    
+    p -= 2;
+    REQUIRE (*p == 0);
+}
+
 
 #endif
