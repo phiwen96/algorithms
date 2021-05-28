6d72a91e9faaf6919ce55f1b5d3ca03736c9755e
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-05-28 11:42:19 +0200

    a

--- tests/src/test_main.cpp
+++ tests/src/test_main.cpp
@@ -21,24 +21,50 @@ enum opcode : int
     OP_DIVIDE
 };
 
-
-
-
-
-template <auto...>
-struct constant;
-
-template <auto T>
-requires requires ()
+struct token
 {
-    requires (sizeof (T) == 1);
-}
-struct constant <T>
-{
-    using value_type = decltype (T);
-    cexp value_type value = T;
+    enum struct type
+    {
+        //single-character tokens
+        TOKEN_LEFT_PAREN, TOKEN_RIGHT_PAREN,
+        TOKEN_LEFT_BRACE, TOKEN_RIGHT_BRACE,
+        TOKEN_COMMA, TOKEN_DOT, TOKEN_MINUS, TOKEN_PLUS,
+        TOKEN_SEMICOLON, TOKEN_SLASH, TOKEN_STAR,
+        
+        //one or two character tokens
+        TOKEN_BANG, TOKEN_BANG_EQUAL,
+        TOKEN_EQUAL, TOKEN_EQUAL_EQUAL,
+        TOKEN_GREATER, TOKEN_GREATER_EQUAL,
+        TOKEN_LESS, TOKEN_LESS_EQUAL,
+        
+        //literals
+        TOKEN_IDENTIFIER, TOKEN_STRING, TOKEN_NUMBER,
+        
+        //keywords
+        TOKEN_AND, TOKEN_CLASS, TOKEN_ELSE, TOKEN_FALSE,
+        TOKEN_FOR, TOKEN_FUN, TOKEN_IF, TOKEN_NIL, TOKEN_OR,
+        TOKEN_PRINT, TOKEN_RETURN, TOKEN_SUPER, TOKEN_THIS,
+        TOKEN_TRUE, TOKEN_VAR, TOKEN_WHILE,
+        
+        TOKEN_ERROR, TOKEN_EOF
+    };
     
+    type t;
+    char const* start;
+    int length;
+    int line;
 };
+
+struct scanner
+{
+    char const* start;
+    char const* current;
+    int line;
+    
+    scanner (char const* source) : start {source}, current {source}, line {1} {}
+};
+
+
 
 
 
@@ -196,26 +222,30 @@ TEST_CASE ("interface")
     auto code   = array <int> {};
     auto consts = array <int> {};
     
-    code += opcode::OP_CONSTANT;
-    consts += 10;
-    code += 0;
+    [&]{
+        code += opcode::OP_CONSTANT;
+        consts += 10;
+        code += consts.active() - 1;
+        
+        code += opcode::OP_CONSTANT;
+        consts += 20;
+        code += consts.active() - 1;
+        code += opcode::OP_NEGATE;
+        
+        code += opcode::OP_CONSTANT;
+        consts += 1;
+        code += consts.active() - 1;
+        
+        code += opcode::OP_CONSTANT;
+        consts += 2;
+        code += consts.active() - 1;
+        
+        code += opcode::OP_ADD;
+        
+        code += opcode::OP_RETURN;
+    }();
     
-    code += opcode::OP_CONSTANT;
-    consts += 20;
-    code += 1;
-    code += opcode::OP_NEGATE;
     
-    code += opcode::OP_CONSTANT;
-    consts += 1;
-    code += 2;
-    
-    code += opcode::OP_CONSTANT;
-    consts += 2;
-    code += 3;
-    
-    code += opcode::OP_ADD;
-    
-    code += opcode::OP_RETURN;
     
     
     
