a7f01dd45d447e8fc383cfa7767529d8707797f8
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-05-29 14:26:13 +0200

    a

--- tests/src/test_main.cpp
+++ tests/src/test_main.cpp
@@ -55,13 +55,20 @@ struct token
     int line;
 };
 
+
+/*
+ scanner doesn’t report lexical errors. Instead, it creates special error tokens and leaves it up to the parser to report them.
+ */
 struct scanner
 {
     char const* start;
     char const* current;
     int line;
     
-    scanner (char const* source) : start {source}, current {source}, line {1} {}
+    scanner (char const* source) : start {source}, current {source}, line {1}
+    {
+        
+    }
     
     auto scan_token () -> token
     {
@@ -79,6 +86,10 @@ struct scanner
         if (isdigit (c))
         {
             return number ();
+            
+        } else if (is_alpha (c))
+        {
+            return identifier ();
         }
         
         
@@ -108,9 +119,76 @@ struct scanner
         
         return make_error_token ("Unexpected character.");
     }
-
+    
     
 private:
+    auto identifier_type () const -> token::type
+    {
+        switch (start [0]) {
+            case 'a': return check_keyword (1, 2, "nd", token::type::TOKEN_AND);
+            case 'c': return check_keyword (1, 4, "lass", token::type::TOKEN_CLASS);
+            case 'e': return check_keyword (1, 3, "lse", token::type::TOKEN_ELSE);
+            case 'f':
+                if (current - start > 1)
+                {
+                    switch (start[1])
+                    {
+                        case 'a': return check_keyword(2, 3, "lse", token::type::TOKEN_FALSE);
+                        case 'o': return check_keyword(2, 1, "r", token::type::TOKEN_FOR);
+                        case 'u': return check_keyword(2, 1, "n", token::type::TOKEN_FUN);
+                    }
+                }
+                break;
+            case 'i': return check_keyword (1, 1, "f", token::type::TOKEN_IF);
+            case 'n': return check_keyword (1, 2, "il", token::type::TOKEN_NIL);
+            case 'o': return check_keyword (1, 1, "r", token::type::TOKEN_OR);
+            case 'p': return check_keyword (1, 4, "rint", token::type::TOKEN_PRINT);
+            case 'r': return check_keyword (1, 5, "eturn", token::type::TOKEN_RETURN);
+            case 's': return check_keyword (1, 4, "uper", token::type::TOKEN_SUPER);
+            case 't':
+                if (current - start > 1)
+                {
+                    switch (start [1])
+                    {
+                        case 'h': return check_keyword (2, 2, "is", token::type::TOKEN_THIS);
+                        case 'r': return check_keyword (2, 2, "ue", token::type::TOKEN_TRUE);
+                    }
+                }
+                break;
+            case 'v': return check_keyword (1, 2, "ar", token::type::TOKEN_VAR);
+            case 'w': return check_keyword (1, 4, "hile", token::type::TOKEN_WHILE);
+        }
+        
+        return token::type::TOKEN_IDENTIFIER;
+    }
+    
+    auto check_keyword (int start, int length, const char* rest, token::type t) const -> token::type
+    {
+        if (this->current - this->start == start + length && memcmp (this->start + start, rest, length) == 0)
+        {
+            return t;
+        }
+        
+        return token::type::TOKEN_IDENTIFIER;
+    }
+    
+    auto is_alpha (char c) const -> bool
+    {
+        return (c >= 'a' and c <= 'z') or
+        (c >= 'A' and c <= 'Z') or
+        (c == '_');
+    }
+    
+    auto identifier () -> token
+    {
+        while (is_alpha (peek ()) || isdigit (peek ()))
+        {
+            advance ();
+        }
+        
+        return make_token (identifier_type ());
+    }
+    
     auto number () -> token
     {
         while (isdigit (peek ()))
@@ -178,7 +256,7 @@ private:
                         return;
                     }
                 }
-
+                    
                 default:
                     return;
             }
@@ -398,6 +476,166 @@ private:
     }
 };
 
+struct parser
+{
+    token current;
+    token previous;
+    bool had_error {false};
+    bool panic_mode {false};
+};
+
+/**
+ The compiler will take the user’s program and fill up the chunk with bytecode.
+ At least, that’s what it will do if the program doesn’t have any compile errors.
+ 
+ A compiler has roughly two jobs. It parses the user’s source code to understand what it means. Then it takes that knowledge and outputs low-level instructions that produce the same semantics.
+ */
+struct compiler
+{
+    array <int> code;
+    array <int> consts;
+    scanner sc;
+    parser prs;
+    
+    compiler (char const* source) : sc {source}
+    {
+        advance ();
+        expression ();
+        consume (token::type::TOKEN_EOF, "Expect end of expression.");
+    }
+    
+    // returns whether or not compilation succeeded
+    operator bool () const
+    {
+        return not prs.had_error;
+    }
+    
+private:
+    auto unary() -> void
+    {
+      token::type operatorType = prs.previous.t;
+
+      // Compile the operand.
+      expression ();
+
+      // Emit the operator instruction.
+      switch (operatorType)
+      {
+          case token::type::TOKEN_MINUS: emit_bytes (opcode::OP_NEGATE); break;
+          default: return; // Unreachable.
+      }
+    }
+    
+    auto grouping() -> void
+    {
+      expression ();
+      consume (token::type::TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+    }
+    
+    auto number() -> void
+    {
+      int value = strtod (prs.previous.start, NULL);
+      emit_constants (value);
+    }
+    
+    auto emit_constants(int value) -> void
+    {
+      emit_bytes (opcode::OP_CONSTANT, make_constant (value));
+    }
+    
+    auto make_constant (int value) -> int
+    {
+        consts += value;
+        int index = consts.active ();
+        
+//      if (constant > UINT8_MAX) {
+//        error("Too many constants in one chunk.");
+//        return 0;
+//      }
+
+      return index;
+    }
+    
+    auto expression () -> void
+    {
+        
+    }
+    
+    auto emit_return (int b) -> void
+    {
+        emit_bytes (opcode::OP_RETURN);
+    }
+    
+    auto emit_bytes (auto... b) -> void
+    {
+        ((code += b), ...);
+    }
+    
+    /*
+     It asks the scanner for the next token and stores it for later use.
+     Before doing that, it takes the old current token and stashes that in a previous field.
+     */
+    auto advance () -> void
+    {
+        prs.previous = prs.current;
+        
+        prs.current = sc.scan_token ();
+        
+        if (prs.current.t == token::type::TOKEN_ERROR)
+        {
+            throw;
+        }
+    }
+    
+    auto consume (token::type t, const char* msg) -> void
+    {
+      if (prs.current.t == t)
+      {
+        advance ();
+        return;
+      }
+
+      error_at_current (msg);
+    }
+    
+    auto error_at_current(const char* msg) -> void
+    {
+        error_at (&prs.current, msg);
+    }
+    
+    auto error(const char* msg) -> void
+    {
+        error_at (&prs.previous, msg);
+    }
+    
+    auto error_at (token* tok, const char* msg) -> void
+    {
+        if (prs.panic_mode)
+        {
+            return;
+        }
+        
+        prs.panic_mode = true;
+        
+        fprintf (stderr, "[line %d] Error", tok->line);
+        
+        if (tok->t == token::type::TOKEN_EOF)
+        {
+            fprintf(stderr, " at end");
+            
+        } else if (tok->t == token::type::TOKEN_ERROR)
+        {
+            // Nothing.
+        } else
+        {
+            fprintf(stderr, " at '%.*s'", tok->length, tok->start);
+        }
+        
+        fprintf (stderr, ": %s\n", msg);
+        prs.had_error = true;
+    }
+};
+
 
 
 TEST_CASE ("interface")
@@ -456,13 +694,16 @@ TEST_CASE ("interface")
         default:
             break;
     }
-
+    
 }
 
 
 TEST_CASE ("")
 {
+    auto code   = array <int> {};
+    auto consts = array <int> {};
     
+    auto v = vm {code, consts};
 }
 
 
