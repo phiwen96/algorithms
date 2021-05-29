0c24ee49f91bb79aae15410ee17e9d0a2fbfe694
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-05-29 16:54:10 +0200

    a

--- include/algorithms/parser.hpp
+++ include/algorithms/parser.hpp
@@ -8,3 +8,28 @@ struct parser
     bool had_error {false};
     bool panic_mode {false};
 };
+
+enum precedence
+{
+  PREC_NONE,
+  PREC_ASSIGNMENT,  // =
+  PREC_OR,          // or
+  PREC_AND,         // and
+  PREC_EQUALITY,    // == !=
+  PREC_COMPARISON,  // < > <= >=
+  PREC_TERM,        // + -
+  PREC_FACTOR,      // * /
+  PREC_UNARY,       // ! -
+  PREC_CALL,        // . ()
+  PREC_PRIMARY
+};
+
+
+using parse_fn =  void (*)();
+
+struct parse_rule
+{
+    parse_fn prefix;
+    parse_fn infix;
+    parse_fn precedence;
+};
--- include/algorithms/token.hpp
+++ include/algorithms/token.hpp
@@ -1,15 +1,57 @@
 #pragma once
 
+#define TOKEN \
+    X (TOKEN_LEFT_PAREN) \
+    X (TOKEN_RIGHT_PAREN) \
+    X (TOKEN_LEFT_BRACE) \
+    X (TOKEN_RIGHT_BRACE) \
+    X (TOKEN_COMMA) \
+    X (TOKEN_DOT) \
+    X (TOKEN_MINUS) \
+    X (TOKEN_PLUS) \
+    X (TOKEN_SEMICOLON) \
+    X (TOKEN_SLASH) \
+    X (TOKEN_STAR) \
+    X (TOKEN_BANG) \
+    X (TOKEN_BANG_EQUAL) \
+    X (TOKEN_EQUAL) \
+    X (TOKEN_EQUAL_EQUAL) \
+    X (TOKEN_GREATER) \
+    X (TOKEN_GREATER_EQUAL) \
+    X (TOKEN_LESS) \
+    X (TOKEN_LESS_EQUAL) \
+    X (TOKEN_IDENTIFIER) \
+    X (TOKEN_STRING) \
+    X (TOKEN_NUMBER) \
+    X (TOKEN_AND) \
+    X (TOKEN_CLASS) \
+    X (TOKEN_ELSE) \
+    X (TOKEN_FALSE) \
+    X (TOKEN_FOR) \
+    X (TOKEN_FUN) \
+    X (TOKEN_IF) \
+    X (TOKEN_NIL) \
+    X (TOKEN_OR) \
+    X (TOKEN_PRINT) \
+    X (TOKEN_RETURN) \
+    X (TOKEN_SUPER) \
+    X (TOKEN_THIS) \
+    X (TOKEN_TRUE) \
+    X (TOKEN_VAR) \
+    X (TOKEN_WHILE) \
+    X (TOKEN_ERROR) \
+    X (TOKEN_EOF)
+
 struct token
 {
-    enum struct type
+    enum type
     {
         //single-character tokens
         TOKEN_LEFT_PAREN, TOKEN_RIGHT_PAREN,
         TOKEN_LEFT_BRACE, TOKEN_RIGHT_BRACE,
         TOKEN_COMMA, TOKEN_DOT, TOKEN_MINUS, TOKEN_PLUS,
         TOKEN_SEMICOLON, TOKEN_SLASH, TOKEN_STAR,
-        
+         
         //one or two character tokens
         TOKEN_BANG, TOKEN_BANG_EQUAL,
         TOKEN_EQUAL, TOKEN_EQUAL_EQUAL,
@@ -33,3 +75,4 @@ struct token
     int length;
     int line;
 };
+
